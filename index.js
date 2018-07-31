717bd0fa104b934c1467b1c6d99f224e5857fd71
Matt Miller <matt123miller@hotmail.co.uk>
2018-07-31 21:04:38 +0100

    This is the shell of my structure.

--- index.js
+++ index.js
@@ -1,2 +1,14 @@
-console.log('hello');
+const express = require('express');
+require('dotenv').config({ path: '.env' });
 
+const app = new express();
+
+app.get('/', (req, res) => res.send('Hello World!'));
+
+//TODO: When we have a static url later use /cowsay
+app.post('/', postHandler);
+
+
+app.listen(process.env.PORT, () => console.log(`Example app listening on port ${process.env.PORT}`));
+function postHandler(req, res) {
+}
